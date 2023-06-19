0cf6c59364bf0b4b28e1dd96212e8390b77b7e0f
Prashanthg01 <prashanthg9845@gmail.com>
2023-06-19 23:06:11 +0530

    commited

--- main.py
+++ main.py
@@ -1,34 +1,74 @@
-import random as rd
-count = 0
-win = 0
-loss = 0
-name = input("enter your name: ")
-while True:
-    count = count + 1
-    n = input('choose: rock, paper or scissors: ')
-    n = n.lower()
-    x = rd.choice(['rock', 'paper', 'scissors'])
-    print(f'The computer choise is {x}')
-    print(f'number of chances remaining {count}')
-    if n == x:
-        print("You tied")
-        count = count-1
-        continue
-    elif n == 'paper' and x == 'rock':
-        win = win + 1
-        print(f'You Win {win} times')
-    elif n == 'rock' and x == 'scissors':
-        win = win + 1
-        print(f'You Win {win} times')
-    elif n == 'scissors' and x == 'paper':
-        win = win + 1
-        print(f'You Win {win} times')
+import random
+
+choices = ['rock', 'paper', 'scissors', 'lizard', 'Spock']
+
+def determine_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return 'tie'
+    elif (player_choice == 'rock' and computer_choice in ['scissors', 'lizard']) or \
+         (player_choice == 'paper' and computer_choice in ['rock', 'Spock']) or \
+         (player_choice == 'scissors' and computer_choice in ['paper', 'lizard']) or \
+         (player_choice == 'lizard' and computer_choice in ['paper', 'Spock']) or \
+         (player_choice == 'Spock' and computer_choice in ['rock', 'scissors']):
+        return 'player'
     else:
-        print(f'You Loss try again {loss} times')
-    loss = loss+1
-    if count==5 and win>=3:
-        count = 0
-        print("you are the finall winner")
-    if count==5 and win<3:
-        count = 0
-        print(f"{name} you loss")
+        return 'computer'
+
+def print_choices():
+    print("Available choices: ")
+    for i, choice in enumerate(choices):
+        print(f"{i + 1}. {choice}")
+
+def print_score(player_score, computer_score):
+    print(f"Score - Player: {player_score}  Computer: {computer_score}")
+
+def play_game():
+    player_score = 0
+    computer_score = 0
+    total_rounds = 0
+
+    print("Welcome to Rock Paper Scissors Lizard Spock!")
+    name = input("Enter your name: ")
+
+    while True:
+        print_choices()
+        player_choice = input("Enter your choice (1-5): ")
+        if player_choice.isdigit() and 1 <= int(player_choice) <= 5:
+            player_choice = choices[int(player_choice) - 1]
+        else:
+            print("Invalid choice. Please try again.")
+            continue
+
+        computer_choice = random.choice(choices)
+
+        print(f"{name}'s choice: {player_choice}")
+        print(f"Computer's choice: {computer_choice}")
+
+        winner = determine_winner(player_choice, computer_choice)
+
+        if winner == 'tie':
+            print("It's a tie!")
+        elif winner == 'player':
+            print(f"{name} wins!")
+            player_score += 1
+        else:
+            print("Computer wins!")
+            computer_score += 1
+
+        total_rounds += 1
+        print_score(player_score, computer_score)
+
+        if total_rounds >= 5:
+            if player_score > computer_score:
+                print(f"{name} is the winner!")
+            elif player_score < computer_score:
+                print("Computer is the winner!")
+            else:
+                print("It's a tie overall!")
+            break
+
+        play_again = input("Play again? (y/n): ")
+        if play_again.lower() != 'y':
+            break
+
+play_game()
